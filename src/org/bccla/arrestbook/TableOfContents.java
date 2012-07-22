7a744b349d23c384051093552572e2a2c6c60452
Paul H <pholden@matrix.(none)>
2012-07-21 23:55:55 -0400

    Start BCCLA Arrest Pocketbook

---
+++ src/org/bccla/arrestbook/TableOfContents.java
@@ -0,0 +1,15 @@
+package org.bccla.arrestbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TableOfContents extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.toc);
+    }
+}
