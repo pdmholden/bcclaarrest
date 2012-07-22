3953bdb6e0db0d95f00a3df97ef76888ecd6d638
Paul H <pholden@matrix.(none)>
2012-07-22 15:36:31 -0400

    Create basic TOC

--- src/org/bccla/arrestbook/TableOfContents.java
+++ src/org/bccla/arrestbook/TableOfContents.java
@@ -1,15 +1,30 @@
 package org.bccla.arrestbook;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class TableOfContents extends Activity
+public class TableOfContents extends ListActivity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.toc);
+
+        // get the data (from where?)
+        String[] chapters = new String[] {
+            "Important Notice and Qualification",
+            "Introduction",
+            "Just Making Conversation",
+            "Police are Detaining You",
+            "Being Detained",
+            "Police are Arresting You",
+            "When You are Arrested" };
+
+        // set the adapter & view (not using XML)
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, chapters);
+        setListAdapter(adapter);
     }
 }
