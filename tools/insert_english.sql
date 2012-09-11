ab4889ad17d59a90877ce1e0846b2818d1af7d5a
Paul Holden <pdmholden@gmail.com>
2012-09-11 14:26:05 -0700

    Put English text into separate SQL file from DB schema
    Note that English text does not match publicly available PDF version. Text will
    need to be revised.

---
+++ tools/insert_english.sql
@@ -0,0 +1,72 @@
+BEGIN TRANSACTION;
+
+INSERT INTO book_content(title, content) VALUES("Important Notice and Qualification", "This pocketbook has been prepared and published for information and educational purposes only. It is not legal advice and it is not intended that this pocketbook should in any way replace legal advice from a qualified lawyer. Individuals with specific legal problems should seek legal advice from a qualified lawyer. See the list of contact numbers at the back of this pocketbook to locate a lawyer near you.");
+INSERT INTO book_content(title, content) VALUES("Introduction", "The B.C. Civil Liberties Association (BCCLA) is the oldest and most active civil liberties group in Canada. With the support of the Law Foundation of B.C., we have published this pocketbook to help you understand your rights and responsibilities when you are dealing with the police.
+
+The police are responsible for enforcing the law. While they are deserving of respect and assistance, they must also respect your legal rights. This pocketbook has important information about how you can expect the police to act, and how you can respond if you feel the police do not act properly.
+
+If you need specific legal advice, talk to a lawyer. This book is a pocket-sized, shorter version of the full-length BCCLA Arrest Handbook.");
+INSERT INTO book_content(title, content) VALUES("Just Making Conversation", "There are three reasons why a police officer would stop someone walking down the street. You have different rights in each situation.
+
+Reason #1 - The Police are Just Making Conversation
+
+Police officers can use polite conversation to find a reason to hold you or arrest you. They could be investigating an incident you saw or know about. They might also simply be friendly. If you don't like the questions the police are asking, ask: ""Am I free to go?"" If the answer is yes, you can leave. If the answer is no, you are being detained (see Reason #2).
+
+Reason #2 - The Police are Investigating You (Detention)
+
+If you ask if you are free to go and the police officer says no, you have the right to be told why you are being detained. Remember the reason the police officer gives you, if he gives you one. Get the badge number or name of the police officer so that you will have it if you want to make a complaint.
+
+If you are being detained but you are not under arrest, you aren't driving, and you haven't broken any laws, you don't even have to give the police officer your name. If you don't want to talk, say: ""I want to remain silent."" You may want to tell the police your name and address to show you are cooperative.");
+INSERT INTO book_content(title, content) VALUES("Being Detained", "If you have not been arrested, but you are being held against your will, remember to:
+
+- Ask why you are being held.
+- Get the badge number or name of the police officer.
+- Stay silent and tell the police you are staying silent (if you feel safe doing so). You may, if you have not committed any crime, wish to explain what you were doing that made the police suspicious. Sometimes an explanation can end a police interaction more quickly.
+- Tell the police if you have needles or other sharp objects with you before they search you. They'll probably find the needles anyway, and you don't want to accidentally hurt a police officer.
+
+Reason #3 - The Police are Arresting You
+
+You will know if you are under arrest because a police officer has said you are under arrest, or somehow indicated you are not free to go by physically holding you. If you are under arrest, and the police ask, you must tell them your name and address. The police must tell you why you are being arrested unless the reason is obvious - remember what they say so you can tell your lawyer.
+
+The arresting officer may release you right away with an ""appearance notice"" (a sheet of paper that tells you where and when to show up for court) if she believes that you will show up for court, not break any more laws, and she is sure she knows who you are.");
+INSERT INTO book_content(title, content) VALUES("When You are Arrested", "- The police can do a full search of you and your personal property.
+
+- You may be kept in jail for up to 24 hours before appearing before a judge or justice of the peace.
+
+- Any pulling, kicking, punching, running or physical action to avoid arrest could result in more charges. Going limp does not count as resistance.
+
+- The police are allowed to use only as much force as is necessary to arrest you or ensure that the situation is safe. Any additional force is considered 'excessive' force.
+
+- The police may keep asking questions, but you don't have to answer them. Tell the police you want to remain silent and you want to speak to a lawyer.
+
+- Giving a fake name or address could result in more charges.");
+INSERT INTO book_content(title, content) VALUES("Reasons for Arrest", "The law says the police must have a reason for arresting you. You can only be arrested by the police if:
+
+- A police officer sees you committing a criminal offence.
+
+- A police officer has reasonable grounds to believe you have committed or are about to commit an indictable or hybrid offence (p. 11).
+
+- You have broken any law (including provincial laws or city bylaws) and you won't tell police who you are and where you live.
+
+- There is a warrant for your arrest (p. 12).
+
+- A police officer has a reason to think you have a mental disorder and are dangerous (p. 10).
+
+- You have breached the peace or are about to breach the peace (p. 13).
+
+- You are drunk or high in public (p. 13).
+
+- A police officer has a reason to think you are a terrorist about to commit a terrorist act.");
+INSERT INTO book_content(title, content) VALUES("Types of Offences", "There are three types of offences:
+
+- Summary Offences = The least serious offences. They generally have lower penalties.
+
+- Hybrid Offences = Where the Crown counsel can choose whether the offence is a summary or indictable offence depending on the situation.
+
+- Indictable Offences = The most serious offences. They have much higher penalties.
+
+For hybrid and summary offences, a police officer must release you immediately after arrest with an appearance notice (see page 6) unless she: (a) can't identify you; (b) needs to preserve the evidence of your crime; (c) needs to stop you from committing the crime or repeating it; or (d) has reasonable grounds to believe you won't come to court.");
+INSERT INTO book_content(title, content) VALUES("Arrest Warrants", "A warrant is a piece of paper that a judge uses to allow the police to do something. Arrest warrants order the police to arrest a specific person. If the police have a warrant for your arrest, ask to see it.  You have the right to see the warrant as soon as possible. When you read the warrant, make sure the warrant is actually for you.");
+INSERT INTO book_content(title, content) VALUES("The Mental Health Act", "Under the Mental Health Act, police officers can use force to take you to a hospital for an exam by a doctor if they think that you are acting in a way that is dangerous to yourself or other people, and you seem to have a mental disorder that needs treatment.  You can request a review of your detention. Ask staff members at the hospital for the review panel request form. You can also contact the Mental Health Law Program for legal information and advice.");
+
+COMMIT;
