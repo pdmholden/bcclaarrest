d18bcd99dd038d628acbc17a2b3f8dcd3ff64b94
Paul Holden <pdmholden@gmail.com>
2012-11-17 18:00:20 -0500

    Various changes to implement upgrade to 0.2
    - Move DB stuff into onResume() & run importAssets() on DB upgrade
    - Move importAssets() into PocketbookContent FWIW
    - DB/App version change

--- src/org/bccla/arrest/PocketbookContent.java
+++ src/org/bccla/arrest/PocketbookContent.java
@@ -1,33 +1,98 @@
 package org.bccla.arrest;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 class PocketbookContent extends SQLiteOpenHelper
 {
+    private final String TAG = "PocketbookContent";
+    private final Context mCtx;
+
+    private final String DB_DIR = "/data/data/org.bccla.arrest/databases";
     private static final String DB_NAME = "arrest_pocketbook.sqlite";
-    private static final int DB_SCHEMA_VER = 1;
+    private static final int ORIG_REL_SCHEMA_VER = 1;
+    private static final int DB_SCHEMA_VER = 2;
 
     public PocketbookContent(Context ctx)
     {
         super(ctx, DB_NAME, null, DB_SCHEMA_VER);
+        mCtx = ctx;
+
+        try
+        {
+            importAssets(false);
+        }
+        catch (IOException ioe)
+        {
+            Log.e(TAG, "importAssets() exception" + ioe.getMessage());
+        }
+    }
+
+    private void importAssets(boolean overWrite) throws IOException
+    {
+        AssetManager am = mCtx.getAssets();
+        InputStream in = am.open(DB_NAME);
+        FileOutputStream out;
+        byte[] buffer = new byte[1024];
+        int len;
+        File dir, db;
+
+        dir = new File(DB_DIR);
+        dir.mkdirs();
+        db = new File(dir, DB_NAME);
+
+        // if not first run return
+        if (!overWrite && db.exists())
+        {
+            Log.i(TAG, DB_DIR + "/" + DB_NAME + " exists, do nothing");
+            in.close();
+            return;
+        }
+
+        // copy the DB from assets to standard DB dir created above
+        out = new FileOutputStream(db);
+        while ((len = in.read(buffer)) > 0)
+        {
+            out.write(buffer, 0, len);
+        }
+
+        out.flush();
+        out.close();
+        in.close();
+        if (!db.exists())
+        {
+            Log.e(TAG, DB_DIR + "/" + DB_NAME + " does not exist");
+        }
     }
 
     @Override
     public void onCreate(SQLiteDatabase db)
     {
-        // should have been imported already from assets
+        // do nothing, this is a stupid funcction
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer)
     {
+        if (ORIG_REL_SCHEMA_VER == oldVer &&
+            DB_SCHEMA_VER == newVer)
+        {
+            try
+            {
+                importAssets(true);
+            }
+            catch (IOException ioe)
+            {
+                Log.e(TAG, "importAssets() exception" + ioe.getMessage());
+            }
+        }
     }
 }
--- src/org/bccla/arrest/TableOfContents.java
+++ src/org/bccla/arrest/TableOfContents.java
@@ -2,90 +2,45 @@ package org.bccla.arrest;
 
 import android.app.ListActivity;
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.IOException;
 
 import org.bccla.arrest.PocketbookContent;
 
 public class TableOfContents extends ListActivity
 {
     public final static String CH_ID = "org.bccla.arrestbook.CH_ID";
-    private final String TAG = "TableOfContents";
-    private final String DB_DIR = "/data/data/org.bccla.arrest/databases";
-    private final String DB_NAME = "arrest_pocketbook.sqlite";
 
-    private void importAssets() throws IOException
-    {
-        AssetManager am = getAssets();
-        InputStream in = am.open(DB_NAME);
-        FileOutputStream out;
-        byte[] buffer = new byte[1024];
-        int len;
-        File dir, db;
-
-        dir = new File(DB_DIR);
-        dir.mkdirs();
-        db = new File(dir, DB_NAME);
-
-        // if not first run return
-        if (db.exists())
-        {
-            Log.i(TAG, DB_DIR + "/" + DB_NAME + " exists, do nothing");
-            in.close();
-            return;
-        }
-
-        // copy the DB from assets to standard DB dir created above
-        out = new FileOutputStream(db);
-        while ((len = in.read(buffer)) > 0)
-        {
-            out.write(buffer, 0, len);
-        }
-
-        out.flush();
-        out.close();
-        in.close();
-        if (!db.exists())
-        {
-            Log.e(TAG, DB_DIR + "/" + DB_NAME + " does not exist");
-        }
-    }
+    private PocketbookContent mContent;
+    private SQLiteDatabase mDB;
+    private Cursor mRows;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+    }
 
-        try
-        {
-            importAssets();
-        }
-        catch (IOException ioe)
-        {
-            Log.e(TAG, ioe.getMessage());
-            Toast noDB = Toast.makeText(this, R.string.db_not_loaded,
-                Toast.LENGTH_LONG);
-            noDB.show();
-            finish();
-        }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
 
-        // PDMH: move database stuff into onResume?
-        PocketbookContent content = new PocketbookContent(this);
-        SQLiteDatabase db = content.getReadableDatabase();
+        mContent = new PocketbookContent(this);
+        mDB = mContent.getReadableDatabase();
 
-        Cursor rows = db.query("book_content",
+        // PDMH: need check for db existence & finish()
+        //    Toast noDB = Toast.makeText(mCtx, R.string.db_not_loaded,
+        //        Toast.LENGTH_LONG);
+        //    noDB.show();
+        //    finish();
+
+        mRows = mDB.query("book_content",
             new String[] { "id", "title" },
             null,
             null,
@@ -94,23 +49,28 @@ public class TableOfContents extends ListActivity
             "id",
             null);
 
-        String[] chapters = new String[rows.getCount()];
-        rows.moveToFirst();
-        for (int i = 0; i < rows.getCount(); i++)
+        String[] chapters = new String[mRows.getCount()];
+        mRows.moveToFirst();
+        for (int i = 0; i < mRows.getCount(); i++)
         {
             // assume title is in column 1
-            chapters[i] = rows.getString(1);
-            rows.moveToNext();
+            chapters[i] = mRows.getString(1);
+            mRows.moveToNext();
         }
 
-        rows.close();
-        db.close();
-        content.close();
-
         // set the adapter & insert data into view
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        mAdapter = new ArrayAdapter<String>(this,
             R.layout.toc_row, chapters);
-        setListAdapter(adapter);
+        setListAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        mRows.close();
+        mDB.close();
+        mContent.close();
     }
 
     @Override
