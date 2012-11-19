141541a8fa78dfb3b19f5bf1968336293adbff56
Paul Holden <pdmholden@gmail.com>
2012-11-19 13:06:54 -0500

    Add missing content for pocketbook

--- tools/insert_english.sql
+++ tools/insert_english.sql
@@ -54,4 +54,71 @@ INSERT INTO book_content(title, content) VALUES("Types of Offences", "There are 
 For hybrid and summary offences, a police officer must release you immediately after arrest with an appearance notice (see page 6) unless she: (a) can’t identify you; (b) needs to preserve the evidence of your crime; (c) needs to stop you from committing the crime or repeating it; or (d) has reasonable grounds to believe you won’t come to court.");
 INSERT INTO book_content(title, content) VALUES("Arrest Warrants", "A warrant is a piece of paper that a judge uses to allow the police to do something. Arrest warrants order the police to arrest a specific person. If the police have a warrant for your arrest, ask to see it.  You have the right to see the warrant as soon as possible. When you read the warrant, make sure the warrant is actually for you.");
 INSERT INTO book_content(title, content) VALUES("The Mental Health Act", "Under the Mental Health Act, police officers can use force to take you to a hospital for an exam by a doctor if they think that you are acting in a way that is dangerous to yourself or other people, and you seem to have a mental disorder that needs treatment. You can request a review of your detention.  Ask staff members at the hospital for the review panel request form. You can also contact the Mental Health Law Program for legal information and advice.");
+INSERT INTO book_content(title, content) VALUES("Breach of the Peace", "The police can arrest you for breaching the peace.  Breaching the peace means you are causing a disturbance that involves some potential for violence. There is no specific offence of breaching the peace. This means that police must release you after they arrest you unless they are going to charge you for breaking another law.");
+INSERT INTO book_content(title, content) VALUES("Public Intoxication", "Public areas are areas where anyone can go. They include parks, beaches and streets, as well as private places where the public are welcome, such as bars, restaurants and malls. If you are in a public area and you are intoxicated, the police can arrest you.  Intoxicated means that you are so drunk or high that you are unable to care for yourself, a danger to yourself or others, or you are causing a disturbance.  You must be released when you are sober.");
+INSERT INTO book_content(title, content) VALUES("Identifying Yourself to Police", "You only have to give police your name and address if:
+
+•    You are under arrest.
+•    You are driving a car. Passengers in the car do not have to give the police their names or addresses.
+•    You have allegedly broken a law (including a criminal law, a provincial law or a city bylaw).  
+
+What to say to police
+
+•    Do you want to leave? Ask: “Am I free to go?” If yes, then go.
+•    If you are not free to go, ask: “Am I under arrest?” If yes, ask why.
+•    If you are under arrest, say: “I want to remain silent. I want to speak to a lawyer.” Give your name and address if the police ask. Get the police officer’s badge number.
+•    If you are not under arrest, but you can’t leave, ask why. Get the police officer’s badge number.");
+INSERT INTO book_content(title, content) VALUES("Personal Searches", "Most searches happen because people tell the police it is okay to search them. You have the right to say no and the police can’t legally threaten you or force you. If you don’t give permission, the only times the police can search you or your things are when:
+
+•    The police have a reasonable belief (a good reason to believe) that you are carrying drugs, a weapon or open alcohol.
+•    The police have detained you (see page 6) to ask you questions. If you are detained, they may only do a frisk or pat-down search for weapons you could use to hurt them or yourself. They may also look in your bags. They cannot search for needle marks or drugs.
+•    The police have arrested you. If you are arrested, the police can do a full search of your body (not a strip search – unless they have a good reason) and personal possessions.");
+INSERT INTO book_content(title, content) VALUES("Vehicle Searches", "You have different rights when you are driving than when you are walking. When you are driving, police can stop you for many reasons, including to check whether you are impaired. When you are walking, they need a reason they can explain and justify before they can stop you. Just because the police stop your car does not mean they can search you or the car. The police can only search your car if:
+
+1.   You give them permission. You do not have to give police permission.
+2.   You have committed a criminal offence, like drunk or dangerous driving.
+3.   You are arrested.
+4.   They see something sitting in your car in plain view or smell something that raises suspicion and gives them reasonable grounds for the search.");
+INSERT INTO book_content(title, content) VALUES("Seizing Property", "If the police “seize” your property, it means they have taken your property. The police can only take
+your property if it is somehow connected to a crime.
+
+If you have paid for your property with money you got from crime, or you have used it when you were committing a crime, or owning the property itself is a crime, the police can take it. They can also take property that might give them evidence. Police can seize alcohol from you if you are drinking in public, and you are not in a licensed bar or restaurant. Police may also seize unopened alcohol if it is going to be used for an unlawful purpose.
+
+The court should order your lawfully owned property returned once it is no longer valuable as evidence. Your illegally owned property may be destroyed, and will not be returned to you, even if charges are not pursued.");
+INSERT INTO book_content(title, content) VALUES("Security Guards", "The general public can make arrests in some situations. They don’t have the same arrest powers as police.
+
+The police don’t have to see you break the law to arrest you. Members of the general public can only arrest you if they actually see you commit an indictable or hybrid offence or if they see you running away from the police.  People who own property can arrest you if they see you doing something wrong related to their property. They can’t arrest you for being intoxicated in a public place, or for breach of the peace.
+
+Although they may look like police, security guards only have the same rights as regular people, listed above. They can remove you from property they are protecting, just like the person who owns the property can remove you. They can only search you if you give permission.
+
+Citizens and security guards must hand you over to the police as soon as possible after they have arrested you. You are under no obligation to talk to these people.");
+INSERT INTO book_content(title, content) VALUES("Police Accountability", "The police must obey the law. If they act improperly, you can sue them in a civil lawsuit, and you can file a complaint that may result in criminal charges against the police officer. Civil lawsuits are expensive, and you will probably need a lawyer.  Filing complaints is free.
+
+For more information on how to file a police complaint, contact the BCCLA at (604) 687-2919 and ask for a police complaint brochure. There may be time limits to making a police complaint.  Contact the BCCLA or the agencies below for more information about the complaint process.
+
+The Commission for Public Complaints Against the RCMP
+#102-7337 137th St., Surrey, BC, V3W 1A4
+tel 604.501.4080 or 1.800.665.6878
+
+The Office of the Police Complaint Commissioner (City Police)
+#200 – 1111 Melville St., Vancouver, BC, V6E 3V6
+tel 604.660.2385 or 1.800.663.7867");
+INSERT INTO book_content(title, content) VALUES("Important Phone Numbers", "B.C. Civil Liberties Association..............................(604) 687-2919
+Immigration/Refugee Law Clinic............................(604) 601-6390
+Law Line (Legal Services Society)..........................(604) 601-6100
+Lawyer Referral Line.............................................1-800-663-1919
+Legal Aid in Vancouver.........................................(604) 601-6300
+Mental Health Law Program..................................(604) 525-2615
+Office of the Police Complaints Commissioner....1-800-663-7867
+Ombudsman (B.C. Provincial)..............................1-800-567-3247
+Prisoner Legal Services..........................................(604) 853-8712
+Privacy Commissioner of BC.................................(250) 387-5629
+Privacy Commissioner of Canada.........................1-800-282-1376
+RCMP Police Complaints......................................1-800-665-6878
+Refugee Assistance................................................1-888-622-6337
+Salvation Army Pro Bono Lawyer Program...........(604) 299-3908
+UBC Law Student Legal Advice Program..............(604) 822-5791
+Vancouver Area Network of Drug Users..............(604) 683-8595
+Society to Access Justice (Pro Bono Lawyers).........(604) 482-3195");
+
 COMMIT;
