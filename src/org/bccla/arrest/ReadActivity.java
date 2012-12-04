941b1305df6255270c853d5bf7bb5299215903c1
Paul Holden <pdmholden@gmail.com>
2012-12-03 21:12:43 -0500

    ReadChapter fragmentation

--- src/org/bccla/arrest/ReadActivity.java
+++ src/org/bccla/arrest/ReadActivity.java
@@ -1,75 +1,45 @@
 package org.bccla.arrest;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.widget.Toast;
 
-import org.bccla.arrest.PocketbookContent;
-
-public class ReadActivity extends Activity
+public class ReadActivity extends FragmentActivity
 {
     private final long DB_ID_NOT_FOUND = 1949;
-
-    private PocketbookContent mContent;
-    private SQLiteDatabase mDB;
-    private Cursor mRows;
-    private long idFromIntent;
+    private final String FRAG_TAG =
+        ReadActivity.class.getCanonicalName() + ".fragment";
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
         // get chap id from intent
         Intent caller = getIntent();
-        idFromIntent = caller.getLongExtra(TableOfContents.CH_ID,
+        long id = caller.getLongExtra(TableOfContents.CH_ID,
             DB_ID_NOT_FOUND);
-        if (DB_ID_NOT_FOUND == idFromIntent)
+        if (DB_ID_NOT_FOUND == id)
         {
             Toast burned = Toast.makeText(this, R.string.db_id_bad,
                 Toast.LENGTH_LONG);
             burned.show();
             super.onBackPressed();
         }
-    }
 
-    @Override
-    public void onResume()
-    {
-        super.onResume();
+        setContentView(R.layout.chapter_main);
 
-        mContent = new PocketbookContent(this);
-        mDB = mContent.getReadableDatabase();
-
-        mRows = mDB.query("book_content",
-            new String[] { "id", "content" },
-            "id=?",
-            new String[] { String.valueOf(idFromIntent) },
-            null,
-            null,
-            "id",
-            null);
-
-        // get chap data
-        mRows.moveToFirst();
-        String chapterText = mRows.getString(1);
-
-        // display the chap in view
-        setContentView(R.layout.read_chapter);
-        TextView chText = (TextView) findViewById(R.id.one_chapter);
-        chText.setText(chapterText);
-    }
-
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        mRows.close();
-        mDB.close();
-        mContent.close();
+        FragmentManager fragMgr = getSupportFragmentManager();
+        FragmentTransaction xact = fragMgr.beginTransaction();
+        if (null == fragMgr.findFragmentByTag(FRAG_TAG))
+        {
+            xact.add(R.id.chapter_frame,
+                ReadFragment.newInstance(id),
+                FRAG_TAG);
+        }
+        xact.commit();
     }
 }
---
+++ src/org/bccla/arrest/ReadFragment.java
@@ -0,0 +1,97 @@
+package org.bccla.arrest;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.bccla.arrest.PocketbookContent;
+
+public class ReadFragment extends Fragment
+{
+    // create the obj and instantiate it with the chap id
+    public static ReadFragment newInstance(long id)
+    {
+        Bundle init = new Bundle();
+        init.putLong(TableOfContents.CH_ID, id);
+
+        ReadFragment frag = new ReadFragment();
+        frag.setArguments(init);
+        return frag;
+    }
+
+    private PocketbookContent mContent;
+    private SQLiteDatabase mDB;
+    private Cursor mRows;
+    private long idFromIntent;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // retrive id from saved instance
+        if (null == savedInstanceState)
+        {
+            savedInstanceState = getArguments();
+        }
+        idFromIntent = savedInstanceState.getLong(TableOfContents.CH_ID);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle b)
+    {
+        // inflate the view here, populate the TextView in onResume()
+        return(inflater.inflate(R.layout.chapter_frag, container, false));
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState)
+    {
+        // save chap id
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putLong(TableOfContents.CH_ID, idFromIntent);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        mContent = new PocketbookContent(getActivity());
+        mDB = mContent.getReadableDatabase();
+
+        mRows = mDB.query("book_content",
+            new String[] { "id", "content" },
+            "id=?",
+            new String[] { String.valueOf(idFromIntent) },
+            null,
+            null,
+            "id",
+            null);
+
+        // get chap data
+        mRows.moveToFirst();
+        String chapterText = mRows.getString(1);
+
+        // display the chap in view
+        View v = getView();
+        TextView chText = (TextView) v.findViewById(R.id.one_chapter);
+        chText.setText(chapterText);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        mRows.close();
+        mDB.close();
+        mContent.close();
+    }
+}
