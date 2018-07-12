f7fef03b31bba4ef6fade844e1777ddd35ae91e7
Lucia Lu <luxiyalu@foxmail.com>
2018-07-12 10:29:14 -0700

    Add package react-navigation

--- App.tsx
+++ App.tsx
@@ -1,37 +1,15 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import User from './components/user';
+import { createStackNavigator } from 'react-navigation';
+import HomeScreen from './components/HomeScreen';
 
 export default class App extends React.Component<{}> {
   render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={[
-            {key: "I am a bystander"},
-            {key: "I am going to a protest, what do I need to know?"},
-            {key: "I am a member of a marginalized group"},
-            {key: "I am driving a car"},
-          ]}
-          renderItem={
-            ({item}) => <User name={item.key}></User>
-          }
-        />
-      </View>
-    );
+    return <RootStack/>;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    paddingTop: 50,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+const RootStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
   },
 });
---
+++ components/HomeScreen.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+import User from './User';
+
+export default class HomeScreen extends React.Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={[
+            {key: "I am a bystander"},
+            {key: "I am going to a protest, what do I need to know?"},
+            {key: "I am a member of a marginalized group"},
+            {key: "I am driving a car"},
+          ]}
+          renderItem={
+            ({item}) => <User name={item.key}></User>
+          }
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
