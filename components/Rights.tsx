0a5a62ff565baeeb8638a26f783cefeeabb30445
Vincent Truong <vincent.truong@clio.com>
2018-07-13 11:35:38 -0600

    Add successfull call to db to query Rights for a specific section

--- components/Rights.tsx
+++ components/Rights.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {StyleSheet, Button, View, Text, ScrollView} from 'react-native';
+let SQLite = require('react-native-sqlite-storage')
+
 
 export interface Props {
     name: string;
@@ -16,29 +18,32 @@ export default class Rights extends React.Component<any> {
     constructor(props) {
         super(props)
 
-        //TODO: use this.props.navigation.getParam('sectionId') to query for the content and set the state.content
-
         this.state = {
-            content: 'If you have not been arrested, but you are being held against your will, remember to:\n' +
-            '\n' +
-            '•    Ask why you are being held.\n' +
-            '•    Get the badge number or name of the police officer.\n' +
-            '•    Stay silent and tell the police you are staying silent (if you feel safe doing so). You may, if you have not committed any crime, wish to explain what you were doing that made the police suspicious. Sometimes an explanation can end a police interaction more quickly.\n' +
-            '•    Tell the police if you have needles with you before they search you. They’ll probably find the needles anyway, and you don’t want to accidentally hurt a police officer.\n' +
-            '\n' +
-            'Reason #3 – The Police are Arresting You\n' +
-            '\n' +
-            'You will know if you are under arrest because a police officer has said you are under arrest, or somehow indicated you are not free to go by physically holding you. If you are under arrest, and the police ask, you must tell them your name and address. The police must tell you why you are being arrested unless the reason is obvious – remember what they say so you can tell your lawyer.\n' +
-            '\n' +
-            'The arresting officer may release you right away with an “appearance notice” (a sheet of paper that tells you where and when to show up for court) if she believes that you will show up for court, not break any more laws, and she is sure she knows who you are.ow up for court, not break any more laws, and she is sure she knows who you are.ow up for court, not break any more laws, and she is sure she knows who you are.ow up for court, not break any more laws, and she is sure she knows who you are.ow up for court, not break any more laws, and she is sure she knows who you are.ow up for court, not break any more laws, and she is sure she knows who you are.ow up for court, not break any more laws, and she is sure she knows who you are.ow up for court, not break any more laws, and she is sure she knows who you are.',
+            content: ''
         }
     }
+
+  componentDidMount() {
+    let sectionId = this.props.navigation.getParam('sectionId');
+
+    let db = SQLite.openDatabase({name : "arrest_pocketbook.sqlite", createFromLocation : 1});
+    db.transaction((tx: any) => {
+      tx.executeSql("SELECT content FROM book_content WHERE id =" + sectionId, [], (tx: any, results: any) => {
+        console.log("Query completed");
+        let rows = results.rows.raw(); // shallow copy of rows Array
+        rows.map(row => console.log(`Content: ${row.content}`));
+        this.setState({
+          content: rows[0].content
+        });
+      });
+    });
+  }
 
     render() {
         return (
           <ScrollView>
             <Text style={styles.title}>
-              {"Section Id: " + this.props.navigation.getParam('sectionId') + " " + this.props.navigation.getParam('title')}
+              {this.props.navigation.getParam('title')}
               </Text>
               <Text style={styles.body}>
                 {this.state.content}
